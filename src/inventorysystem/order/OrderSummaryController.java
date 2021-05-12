34aa53eddb0c64cbfa174746c12775dc71f2efb6
sanjuv <sanjuv@localhost>
2021-05-13 03:19:53 +0530

    order customer fxml

--- src/inventorysystem/order/OrderSummaryController.java
+++ src/inventorysystem/order/OrderSummaryController.java
@@ -7,7 +7,12 @@ package inventorysystem.order;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 
 /**
  * FXML Controller class
@@ -16,12 +21,28 @@ import javafx.fxml.Initializable;
  */
 public class OrderSummaryController implements Initializable {
 
+    @FXML
+    private NumberAxis numOfOrders;
+    @FXML
+    private CategoryAxis category;
+    @FXML
+    private BarChart<String, Number> barchartt;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("order comparision");
+        series.getData().add(new XYChart.Data<>("Laptop", 100));
+        series.getData().add(new XYChart.Data<>("TV", 75));
+        series.getData().add(new XYChart.Data<>("Mobile", 50));
+        
+        barchartt.getData().add(series);
+        
     }    
     
 }
