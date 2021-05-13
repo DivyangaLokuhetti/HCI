9fae537087d7ccc5c8b3c0086b0a25be72d681a4
sanjuv <sanjuv@localhost>
2021-05-13 19:21:28 +0530

    order fxml

---
+++ src/inventorysystem/order/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventorysystem.order;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sanjuv
+ */
+public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    
+    
+}
